4a5d0ff5903e5262248f520418ca5d9bc456c7f7
Suuumin <93477480+SUUUUUMIN@users.noreply.github.com>
2023-11-30 17:09:07 +0900

    [level unrated] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,7 @@
+import math
+def solution(numer1, denom1, numer2, denom2):
+    num=(numer1*denom2)+(numer2*denom1)
+    den=denom1*denom2
+    gcd=math.gcd(num,den)
+    answer = [num/gcd,den/gcd]
+    return answer
