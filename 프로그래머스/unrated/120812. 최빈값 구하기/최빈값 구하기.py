1f4b0bad026fab315776e4e44f8d70714d424560
Suuumin <93477480+SUUUUUMIN@users.noreply.github.com>
2023-11-30 17:08:41 +0900

    [level unrated] Title: 최빈값 구하기, Time: 14.79 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120812. 최빈값 구하기/최빈값 구하기.py
@@ -0,0 +1,15 @@
+def solution(array):
+    #개수array
+    cnt=[0]*(max(array)+1)
+    
+    for i in array:
+        cnt[i]+=1
+    m=0
+    for j in cnt:
+        if j == max(cnt):
+            m+=1
+    
+    if m>1:
+        return -1
+    else:
+        return cnt.index(max(cnt))
