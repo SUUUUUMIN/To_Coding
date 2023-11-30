8802395098499c2a7ff5b0a0701c6ddae5348e03
Suuumin <93477480+SUUUUUMIN@users.noreply.github.com>
2023-11-30 13:54:49 +0900

    [Silver V] Title: 셀프 넘버, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.py
@@ -0,0 +1,10 @@
+def d(n):
+  n=n+sum(map(int,str(n)))
+  return n
+
+nonselfNum=set()
+for i in range(1,10001):
+  nonselfNum.add(d(i))
+for j in range(1,10001):
+  if j not in nonselfNum:
+    print(j)
