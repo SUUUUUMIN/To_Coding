ea5ca1d1de8f55790378ce9094d8e7c78e94c1df
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-17 16:19:38 +0900

    [Silver IV] Title: 좋은 구간, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -4,8 +4,7 @@ li=list(map(int,input().split()))
 num=int(input())
 
 #초기값 설정
-back=front=num
-res=0
+back=front=res=0
 
 #숫자 추가 후 정렬
 if num in li: #리스트 속 동일한 숫자
@@ -14,7 +13,7 @@ else:
   li.append(num)
   li.append(0)
   li.sort()
-  num_idx=li.index(num)             #정렬 후 입력받은 수가 중간에 있을 때
+  num_idx=li.index(num)           
   back=li[num_idx-1]
   front=li[num_idx+1]
   res=(num-back)*(front-num)-1
