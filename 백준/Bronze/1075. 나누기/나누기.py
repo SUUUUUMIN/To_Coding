7f79d201028af482d9c993ae26cac0994d4fe1ac
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-18 10:30:31 +0900

    [Bronze II] Title: 나누기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.py
+++ 백준/Bronze/1075. 나누기/나누기.py
@@ -2,15 +2,19 @@ N=int(input())
 F=int(input())
 
 #뒷자리 두 개 떼고 00으로 채우기
-n=N//100*100
-flag=1          #플래그로 숫자 찾았는지 검색 
+new_N=N//100*100
+flag=1
+
+#F로 나눠지면 플래그가 0으로 변해서 while문 종료
 while flag==1:
-  if n%F==0:
+  if new_N%F==0:  
     flag=0
   else:
-    n+=1
-res=str(n%100)
-if len(res)==1:
+    new_N+=1
+
+#결과를 str으로 변환 후 뒤에 두 개 떼기
+res=str(new_N%100)
+if len(res)==1:     #길이가 1이면 앞에 0 달아주기
   print(f'0{res}')
 else:
   print(res)
