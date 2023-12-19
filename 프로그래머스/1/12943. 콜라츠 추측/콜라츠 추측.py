a503c1ca7cd0a20079a442a073ae55507dee8fbc
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-19 10:50:52 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -6,9 +6,9 @@ def solution(num):
         elif num%2==0: #짝수
             num=num/2
             answer+=1
-        else:        #홀수
+        elif num%2==1: #홀수
             num=num*3+1
             answer+=1
-    if answer>=500:
+    if answer==500:
         answer=-1
     return answer
