0eb1f050a353d50f7b41269e3de851dd95fc46a3
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-19 14:05:18 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.py
+++ 프로그래머스/1/42889. 실패율/실패율.py
@@ -1,18 +1,19 @@
 def solution(N, stages):
     answer = []
-    cnt=[]
     fail={}
-    for i in range(1,N+1):
-        if i in stages:
-            cnt.append(stages.count(i))
-        else:
-            cnt.append(0)
+    #도전자 수
     people=len(stages)
-    for i in range(N):
-        fail[i+1]=cnt[i]/people
-        people-=cnt[i]
     
+    #실패율
+    for level in range(1,N+1):
+        if people!=0:
+            cnt=stages.count(level)
+            fail[level]=cnt/people
+            people-=cnt
+        else:
+            fail[level]=0
+
     answer=list(fail.items())
-    sorted(answer,key=lambda x : (x[1],-x[0]))
-    
-    return answer
+    answer=sorted(answer,key=lambda x: (-x[1],x[0]))
+    res=[answer[i][0] for i in range(len(answer))]
+    return res
