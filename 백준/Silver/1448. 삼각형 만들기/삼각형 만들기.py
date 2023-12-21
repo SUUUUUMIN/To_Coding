3c20aac4ede2f2ca62e55e91a17537e161301298
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-21 13:52:12 +0900

    [Silver III] Title: 삼각형 만들기, Time: 716 ms, Memory: 75996 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.py
@@ -0,0 +1,19 @@
+#시간초과
+import sys
+input=sys.stdin.readline
+#입력
+N=int(input())
+li=[]
+for _ in range(N):
+  li.append(int(input()))
+#내림차순 정렬
+li.sort(reverse=True)
+#큰 3개 비교
+for i in range(N-2):
+  if li[i]<li[i+1]+li[i+2]:
+    s=li[i]+li[i+1]+li[i+2]
+    break
+  else:
+    s=-1
+#출력
+print(s)
