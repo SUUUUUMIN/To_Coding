47ecc87718b52a9106c969a9abe77d7c5104dd84
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-21 11:44:31 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.60 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -1,23 +1,18 @@
 import re
 def solution(files):
-    answer=[]
-    dic=[]
-    for file in files:
-        #숫자
-        idx=re.findall(r'\d+',file)
-        real=int(idx[0])
-
-        #head
-        head=file[:file.index(idx[0])].lower()
-
-        #dic채우기
-        dic.append([head,real,files.index(file)])
+    my_files=[]
+    for idx,s in enumerate(files):
+        #숫자 추출
+        number=re.findall(r'\d+',s)
+        real_number=int(number[0])
+        #head추출
+        head=s[:s.index(number[0])]
+        head=head.lower()
+        #my_file추가
+        my_files.append([head,real_number,idx,s])
 
     #정렬
-    dic.sort(key=lambda x:(x[0],x[1],x[2]))
-
-    #원본파일 출력
-    for i in range(len(dic)):
-        answer.append(files[dic[i][2]])
-        
+    answer=[]
+    my_files.sort(key=lambda x : (x[0],x[1],x[2]))
+    answer=[i[3] for i in my_files]
     return answer
