fdaebd1918cc541ba501f7f8cfb07b5742c55c6c
SUUUUUMIN <wejkjj4@gmail.com>
2023-12-24 19:44:16 +0900

    [Bronze I] Title: 팀 이름 정하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1296. 팀 이름 정하기/팀 이름 정하기.py
@@ -0,0 +1,23 @@
+#입력받기
+name = list(input())
+n = int(input())
+board = {}
+
+for _ in range(n):
+    team = input().rstrip()
+    nowS = 1
+    scores = []
+
+    for item in ['L', 'O', 'V', 'E']:
+        scores.append(team.count(item) +name.count(item))
+
+    for i in range(len(scores)):
+        for j in range(i+1, len(scores)):
+            nowS *= (scores[i] + scores[j])
+
+    board[team] = nowS % 100
+
+#딕셔너리->리스트, 정렬
+s_li=list(board.items())
+s_li.sort(key=lambda x : (-x[1],x[0]))
+print(s_li[0][0])
