2818a5a441987bdd43c1923a43b4ab2b0d963af3
SUUUUUMIN <wejkjj4@gmail.com>
2024-01-02 17:24:54 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
+++ 백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
@@ -1,8 +1,13 @@
 import sys
-inStr, word = sys.stdin.read(), [0 for i in range(26)]
-for s in inStr:
-    if s.islower(): #소문자인지 체크
-        word[ord(s)-97] += 1    # 알파벳을 아스키코드로 변환
+string_in=sys.stdin.read().replace('\n','').replace(' ','')
+#소문자리스트
+word=[0 for _ in range(26)]
+
+#'a'의 아스키코드 :97
+for s in string_in:
+    word[ord(s)-97]+=1
+
+#최대값찾기
 for i in range(26):
-    if word[i] == max(word):
-        print(chr(i+97), end='') 
+  if word[i]==max(word):
+    print(chr(i+97),end='')
