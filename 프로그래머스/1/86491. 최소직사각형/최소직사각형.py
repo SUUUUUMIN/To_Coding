235096ccd5a030c62d85d4a350cfc9df27f253bf
SUUUUUMIN <wejkjj4@gmail.com>
2024-01-04 15:36:19 +0900

    [level 1] Title: 최소직사각형, Time: 3.14 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -0,0 +1,9 @@
+def solution(sizes):
+    long=0
+    short=0
+    for a,b in sizes:
+        if a<b:
+            a,b=b,a
+        long=max(long,a)
+        short=max(short,b)
+    return long*short
