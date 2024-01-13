0ecc9f5d9f3e2c875e16ee3532347662ebf44255
SUUUUUMIN <wejkjj4@gmail.com>
2024-01-13 15:21:39 +0900

    [level Lv.1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/Lv.1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/Lv.1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,12 +1,8 @@
 def solution(n, arr1, arr2):
     answer = []
     for i in range(len(arr1)):
-        b1=format(arr1[i],'b')
-        b2=format(arr2[i],'b')
-        if len(b1)!=n:
-            b1='0'*(n-len(b1))+b1
-        if len(b2)!=n:
-            b2='0'*(n-len(b2))+b2
+        b1=format(arr1[i],'b').rjust(n,'0')
+        b2=format(arr2[i],'b').rjust(n,'0')
         
         temp=''
         for j in range(n):
