df819f68bbcc89e0ba082f7c1f91e9f6468ccd96
SUUUUUMIN <wejkjj4@gmail.com>
2024-01-18 09:58:32 +0900

    [Bronze II] Title: 줄 세우기, Time: 924 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1681. 줄 세우기/줄 세우기.py
+++ 백준/Bronze/1681. 줄 세우기/줄 세우기.py
@@ -1,10 +1,7 @@
 N, L=map(int,input().split())
-label=[]
-idx=1
-while len(label)<N:
-  if str(L) in str(idx):
-    idx+=1
-  else:
-    label.append(idx)
-    idx+=1
-print(max(label))
+idx=0
+while N>0:
+  idx+=1
+  if str(L) not in str(idx):
+    N-=1
+print(idx)
