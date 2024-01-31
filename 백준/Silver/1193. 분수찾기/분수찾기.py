e646c6bf6b3168b0b0fe57b85922181f3f6b5a47
SUUUUUMIN <wejkjj4@gmail.com>
2024-01-31 15:36:16 +0900

    [Silver V] Title: 분수찾기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.py
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -1,16 +1,15 @@
-input_num = int(input())
-
-line = 0
-max_num = 0
-while input_num > max_num:
-    line += 1  
-    max_num += line  
-
-gap = max_num - input_num 
-if line % 2 == 0:
-    top = line - gap
-    under = gap + 1
-else :
-    top = gap + 1
-    under = line - gap
-print(f'{top}/{under}')
+num=int(input())
+line=1
+while num>line:
+  num-=line
+  line+=1
+# 4 => num=1, line=3
+#짝수줄 경우 => 분모 1 감소, 분자 1 증가
+if line%2==0:
+  a=num
+  b=line-num+1
+#홀수줄 경우 => 분모 1 증가, 분자 1 감소
+else:
+  a=line-num+1
+  b=num
+print(f'{a}/{b}')
