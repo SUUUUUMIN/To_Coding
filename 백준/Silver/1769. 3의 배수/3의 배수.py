c770638a05819b68ff25c4bf54d48a2e43e87070
SUUUUUMIN <wejkjj4@gmail.com>
2024-02-08 15:04:18 +0900

    [Silver V] Title: 3의 배수, Time: 156 ms, Memory: 41268 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,12 +1,9 @@
-num=input()
+num=list(map(int,input()))
 cnt=0
 while len(num)>1:
-  temp=0
-  for n in num:
-    temp+=int(n)
+  num=list(map(int,str(sum(num))))
   cnt+=1
-  num=str(temp)
-if int(num)%3==0:
+if num[0]%3==0:
   print(cnt,'YES')
 else:
   print(cnt,'NO')
