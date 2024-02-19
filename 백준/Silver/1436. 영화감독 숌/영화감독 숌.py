5e576768bb8abd488897254145cda88f0988755f
SUUUUUMIN <wejkjj4@gmail.com>
2024-02-19 10:50:43 +0900

    [Silver V] Title: 영화감독 숌, Time: 588 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,7 +1,11 @@
 n=int(input())
 title = 666
 cnt = 0 
-while True:
+if n<=6:
+  print(int(str(n-1)+str(title)))
+
+else:
+  while True:
     if '666' in str(title):
         cnt += 1 
         if cnt == n:
