f40231295fa104cbcf67402f8175070bd5c3e96e
SUUUUUMIN <wejkjj4@gmail.com>
2024-02-21 08:59:31 +0900

    [Silver V] Title: 날짜 계산, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -5,13 +5,8 @@ while True:
   if earth==e and sun==s and moon==m:
     print(cnt)
     break
-  earth+=1
-  sun+=1
-  moon+=1
-  cnt+=1
-  if earth>=16:
-    earth-=15
-  if sun>=29:
-    sun-=28
-  if moon>=20:
-    moon-=19
+  earth+=1; sun+=1
+  moon+=1; cnt+=1
+  if earth>=16: earth-=15
+  if sun>=29: sun-=28
+  if moon>=20: moon-=19
