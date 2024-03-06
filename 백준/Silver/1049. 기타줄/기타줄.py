7b745a196a31694879021eeee453efae8ee3f9ab
SUUUUUMIN <datalover_sumin@naver.com>
2024-03-06 09:12:17 +0900

    [Silver IV] Title: 기타줄, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -0,0 +1,24 @@
+n,m=map(int,input().split())
+package=1001
+solo=1001
+
+for _ in range(m):
+  p,s=map(int,input().split())
+  if p<package:
+    package=p
+  if s<solo:
+    solo=s
+
+money=[]
+if n//6 == 0:
+  money.append(package)
+  money.append(solo*n)
+else:
+  #낱개
+  money.append(solo*n)
+  #패키지+낱개
+  money.append(package*(n//6)+solo*(n%6))
+  #패키지
+  money.append(package*(n//6 if n%6==0 else n//6+1))
+
+print(min(money))
