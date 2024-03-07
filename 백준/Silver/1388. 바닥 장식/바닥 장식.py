dc38e79cd4528dcbb0161b1f382ade1eca0e246c
SUUUUUMIN <datalover_sumin@naver.com>
2024-03-07 11:32:40 +0900

    [Silver IV] Title: 바닥 장식, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1388. 바닥 장식/바닥 장식.py
@@ -0,0 +1,28 @@
+dx=[1,-1]
+def dfs(x,y):
+  # '-' 인 경우
+  if graph[x][y]=='-':
+    graph[x][y]=1 #방문 확인
+    for d in dx: #좌우 확인
+      newy=y+d 
+      if 0<=newy<m and graph[x][newy]=='-':
+        dfs(x,newy)
+  # '|' 인 경우 
+  else:
+    graph[x][y]=1 #방문확인
+    for d in dx: #상하 확인
+      newx=x+d
+      if 0<=newx<n and graph[newx][y]=='|':
+        dfs(newx,y)
+################################################
+n,m=map(int,input().split())
+graph=[list(input()) for _ in range(n)]
+
+cnt=0
+for i in range(n):
+  for j in range(m):
+    if graph[i][j] in ['-','|']:
+      dfs(i,j)
+      cnt+=1
+  
+print(cnt)
