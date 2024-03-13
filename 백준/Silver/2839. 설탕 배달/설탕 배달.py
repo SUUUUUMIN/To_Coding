badc0c6cd55198b0c3323196388f93d6eecac1ce
SUUUUUMIN <datalover_sumin@naver.com>
2024-03-13 14:47:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,11 +1,11 @@
 n=int(input())
-s=0
+sugar=0
 while n>=0:
   if n%5==0:
-    s+=n//5
-    print(s)
+    sugar+=n//5
+    print(sugar)
     break
   n-=3
-  s+=1
+  sugar+=1
 else:
-  print("-1")
+  print(-1)
