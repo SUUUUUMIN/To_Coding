ba467fb559cda272d165b4f52aa0251dd03d63d8
SUUUUUMIN <datalover_sumin@naver.com>
2024-03-20 15:30:21 +0900

    [Silver IV] Title: 좋은 구간, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1059. 좋은 구간/좋은 구간.py
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -1,20 +1,17 @@
-#입력받기
-idx=int(input())
+n=int(input())
 li=list(map(int,input().split()))
 num=int(input())
 
-#초기값 설정
-back=front=res=0
+left=right=res=0
 
-#숫자 추가 후 정렬
-if num in li: #리스트 속 동일한 숫자
+if num in li:
   res=0
 else:
   li.append(num)
   li.append(0)
   li.sort()
-  num_idx=li.index(num)           
-  back=li[num_idx-1]
-  front=li[num_idx+1]
-  res=(num-back)*(front-num)-1
+  idx=li.index(num)
+  left=li[idx-1]
+  right=li[idx+1]
+  res=(num-left)*(right-num)-1
 print(res)
