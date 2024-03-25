ba6ddf846f78f2e21263b44dd045bcc5deda227c
SUUUUUMIN <datalover_sumin@naver.com>
2024-03-25 13:59:08 +0900

    [Silver III] Title: 랩실에서 잘 자요, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.py
+++ 백준/Silver/27446. 랩실에서 잘 자요/랩실에서 잘 자요.py
@@ -1,11 +1,8 @@
 n,m=map(int,input().split())
-page=sorted(list(set(i for i in range(1,n+1))-set(map(int,input().split()))))
-last=0 #최근 페이지
-ink=0
-for p in page:
-  if last:
-    ink+=min(7,(p-last)*2)
-  else:
-    ink+=7
-  last=p
-print(ink)
+p=sorted(set(i for i in range(1,n+1))-set(map(int,input().split())))
+l,i=0,0
+for j in p:
+  if l:i+=min(7,(j-l)*2)
+  else:i+=7
+  l=j
+print(i)
