401ec8b098c1fc366a6506e0499bbc213e7a3ed5
SUUUUUMIN <datalover_sumin@naver.com>
2024-08-21 15:02:58 +0900

    [Silver I] Title: 회의실 배정, Time: 228 ms, Memory: 59068 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -5,10 +5,10 @@ n=int(input())
 time=[list(map(int,input().split())) for _ in range(n)]
 time.sort(key=lambda x:(x[1],x[0]))
 
-cnt=1
-end=time[0][1]
+cnt=0
+end=0
 
-for s,t in time[1:]:
+for s,t in time:
   if s>=end:
     end=t
     cnt+=1
